b59449a8be0e80cec8cd10061557949901c5a065
babashakare <jokerwolfan@gmail.com>
2025-05-19 20:37:36 +0330

    Add files via upload

--- assets/js/form.js
+++ assets/js/form.js
@@ -14,8 +14,27 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
     if (validateForm()) {
         setTimeout(() => {
             this.reset(); // ریست کردن فرم
+            // پنهان کردن فیلد کد معرف به محض ریست فرم
+            updateInviteCodeField();
         }, 1000); // تاخیر ۱ ثانیه‌ای برای مشاهده ارسال
     } else {
         event.preventDefault(); // جلوگیری از ارسال فرم در صورت عدم اعتبار
     }
 });
+
+const enableInviteCodeCheckbox = document.getElementById('enableInviteCode');
+const inviteCodeField = document.getElementById('inviteCodeField');
+
+function updateInviteCodeField() {
+    if (enableInviteCodeCheckbox.checked) {
+        inviteCodeField.style.display = 'block';
+    } else {
+        inviteCodeField.style.display = 'none';
+    }
+}
+
+// فراخوانی اولیه برای تنظیم نمایش فیلد کد معرف در بارگذاری صفحه
+updateInviteCodeField();
+
+// بروزرسانی نمایش فیلد هنگام تغییر چک‌باکس
+enableInviteCodeCheckbox.addEventListener('change', updateInviteCodeField);
